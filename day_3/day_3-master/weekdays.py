b3584bb52ad25a44ba9b2e4af19312be95858edb
nelsonchege <68642235+nelsonchege@users.noreply.github.com>
2021-06-29 22:55:29 +0300

    Delete day_3/day_3-master directory

--- day_3/day_3-master/weekdays.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-# user must specify format
-print('Enter year and month you want to count format:2000-04')
-start_count = input()
-print('Enter same year and the next month format:2000-05 ')
-stop_count = input()
-# .busday_count() method from numpy gets the number of weekdays
-weekday = np.busday_count(start_count, stop_count)
-print(weekday)
